589d7f0eb40b1ddd896a69e7dc29ea63d0e46613
Tran Pham Quoc Vy <90919097+TranVy10@users.noreply.github.com>
2023-09-24 23:22:04 +0700

    Update ManagerContainerForPM.java

--- src/entities/Manager/ManagerContainerForPM.java
+++ src/entities/Manager/ManagerContainerForPM.java
@@ -10,15 +10,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+// Define a ManagerContainerForPM class that implements ManagerContainerForPMInterfaces
 public class ManagerContainerForPM implements ManagerContainerForPMInterfaces {
+    // Define a constant for the file name
     static final String portContainersListFileName = "data/PortContainersList.txt";
+
+    // Implement the start method required by the ManagerContainerForPMInterfaces interface
     @Override
     public void start(PortManager portManager) {
+        // Get the Port associated with the PortManager
         Port port = portManager.getPort();
+
+        // Get the list of Containers in the specified Port
         List<Containers> containersList = Containers.getContainersInPortList(portContainersListFileName, port.getUniqueID());
+
         Scanner scanner = new Scanner(System.in);
         int option;
         do {
+            // Display the menu options
             System.out.println("1. Add container to port");
             System.out.println("2. Add container to vehicle");
             System.out.println("3. Remove container from port");
@@ -26,37 +35,42 @@ public class ManagerContainerForPM implements ManagerContainerForPMInterfaces {
             System.out.println("5. Return");
             System.out.println("Please choose (1-5): ");
             option = scanner.nextInt();
-            if (option==1){
+
+            // Handle user's choice based on the selected option
+            if (option == 1) {
+                // Add a container to the Port
                 Containers.inputContainerToPortForPM(port);
-            } else if (option==2) {
+            } else if (option == 2) {
+                // Add a container to a vehicle
                 Containers.inputContainerToVehicle();
-            } else if (option==3) {
+            } else if (option == 3) {
+                // Remove a container from the Port
                 String containerToDelete;
                 Scanner scanner1 = new Scanner(System.in);
                 System.out.println("Container that you want to delete: ");
                 containerToDelete = scanner1.nextLine();
-                for (Containers containers:containersList){
-                    if (containers.getUniqueID().equals(containerToDelete)){
+                for (Containers containers : containersList) {
+                    if (containers.getUniqueID().equals(containerToDelete)) {
                         Port.removeContainerFromPort(containerToDelete);
                     }
                 }
-            } else if (option==4) {
+            } else if (option == 4) {
+                // Calculate required fuel for a container
                 String containerID;
                 Scanner scanner1 = new Scanner(System.in);
                 System.out.println("Enter containerID: ");
                 containerID = scanner1.nextLine();
-                for (Containers containers1:containersList){
-                    if (containerID.equals(containers1.getUniqueID())){
+                for (Containers containers1 : containersList) {
+                    if (containerID.equals(containers1.getUniqueID())) {
                         System.out.println("Fuel for ship per Km: " + containers1.requiredShipFuelConsumption());
-                        System.out.println("Fuel for truck per Km: "+ containers1.requiredTruckFuelConsumption());
+                        System.out.println("Fuel for truck per Km: " + containers1.requiredTruckFuelConsumption());
                     }
-
                 }
-            } else if (option==5) {
-
+            } else if (option == 5) {
+                // Return to the previous menu
             } else {
                 System.out.println("Please enter a valid number");
             }
-        } while (option!=5);
+        } while (option != 5);
     }
 }
